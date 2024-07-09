a799d466923616f5665e6889d4382cdcdf21d80b
Kuei <a121212106@gmail.com>
2024-07-09 19:36:20 +0800

    create commodity bean

---
+++ src/main/java/com/tia102g5/commodity/model/Commodity.java
@@ -0,0 +1,10 @@
+package com.tia102g5.commodity.model;
+
+import java.io.Serializable;
+
+public class Commodity implements Serializable {
+    private Integer commodityID;
+    private String commodityName;
+    private Integer commodityPrice;
+    private Integer commodityStock;
+}
